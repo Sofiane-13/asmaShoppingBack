2025ada9e01407d5c01eb2745d09007baa5205ae
BELHADJ KACEM <sofiane_belhadj_kacem@outlook.fr>
2019-02-23 10:56:49 +0100

    adding start

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/shooping', {
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://sofiane:sofiane@cluster0-xfbr5.azure.mongodb.net/test?retryWrites=true",
+  {
     useNewUrlParser: true
-});
+  }
+);
 
 const app = express();
 
-require('./startup/routes')(app);
+require("./startup/routes")(app);
 
-
-app.listen(3000, () => console.log('Listning on 3000 ...'));
+app.listen(3000, () => console.log("Listning on 3000 ..."));
